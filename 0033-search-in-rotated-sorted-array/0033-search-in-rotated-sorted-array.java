d78bbd305e93cf00b0da94a9e0ceb1188ce2e82b
Aniket Singh <as5297145@gmail.com>
2026-08-02 11:50:32 +0530

    Time: 0 ms (100%), Space: 43.8 MB (65.95%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int pivot = pivotsearch(nums);
+        if(pivot == -1){
+            return bs(nums,0,nums.length - 1,target);
+        }
+        if(nums[pivot] == target){
+            return pivot;
+        }
+        
+        
+        if(nums[0]<=target){
+            return bs(nums,0,pivot-1,target);
+        }
+        else {
+        return bs(nums,pivot+1,nums.length-1,target);
+        }
+    }
+    public int bs(int[] nums,int start,int end,int target){
+        while(start<=end){
+            int mid = start + (end - start)/2;
+            if(nums[mid]>target){
+                end = mid - 1;
+            }
+            if(nums[mid]<target){
+                start = mid + 1;
+            }
+            if(nums[mid] == target){
+                return mid;
+            }
+        }
+        return -1;
+    }
+    public int pivotsearch(int[] nums){
+        int start = 0;
+        int end = nums.length - 1;
+        while(start<=end){
+            int mid = start + (end - start)/2;
+            if(mid < end && nums[mid] > nums[mid + 1]){
+                return mid;
+            }
+            if( mid > start && nums[mid - 1] > nums[mid] ){
+                return mid - 1;
+            }
+            if(nums[mid] == nums[start] && nums[mid] == nums[end]){
+                return mid;
+            }
+            if(nums[start]>=nums[mid]){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
